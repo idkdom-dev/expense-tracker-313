6b246a9753640fd6012284c1fae2821f60b4d333
dgehrking <dom46618@gmail.com>
2026-04-09 10:40:17 -0500

    Initial commit

---
+++ src/app/expense.ts
@@ -0,0 +1,15 @@
+export interface Expense {
+  id: string;
+  title: string;
+  amount: number;
+  category: ExpenseCategory;
+}
+
+export type ExpenseCategory =
+  | 'Work'
+  | 'Personal'
+  | 'Grocery'
+  | 'Utilities'
+  | 'Shopping'
+  | 'Travel'
+  | 'Food';
